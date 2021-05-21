dc50799300829a772dba252f3d5890a1d3e7ef68
Maxim <1986maxim1508@gmail.com>
2021-05-21 17:19:34 +0300

    Made some corrections

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,19 @@
-// Mobile Menu panel
+/* Mobile Menu panel
 
-// window.addEventListener('DOMContentLoaded', () => {
-//     const menu = document.querySelector('.menu'),
-//     menuItem = document.querySelectorAll('.menu_item'),
-//     hamburger = document.querySelector('.hamburger');
+window.addEventListener('DOMContentLoaded', () => {
+	const menu = document.querySelector('.menu'),
+		menuItem = document.querySelectorAll('.menu_item'),
+		hamburger = document.querySelector('.hamburger');
 
-//     hamburger.addEventListener('click', () => {
-//         hamburger.classList.toggle('hamburger_active');
-//         menu.classList.toggle('menu_active');
-//     });
+	hamburger.addEventListener('click', () => {
+		hamburger.classList.toggle('hamburger_active');
+		menu.classList.toggle('menu_active');
+	});
 
-//     menuItem.forEach(item => {
-//         item.addEventListener('click', () => {
-//             hamburger.classList.toggle('hamburger_active');
-//             menu.classList.toggle('menu_active');
-//         })
-//     })
-// })
+	menuItem.forEach(item => {
+		item.addEventListener('click', () => {
+			hamburger.classList.toggle('hamburger_active');
+			menu.classList.toggle('menu_active');
+		})
+	})
+}) */
