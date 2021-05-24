13441d7133d8ea7bdc3be2c00928c562b6379720
Maxim <1986maxim1508@gmail.com>
2021-05-24 16:25:33 +0300

    Add Tiny slider insted off Slick slider

--- src/js/script.js
+++ src/js/script.js
@@ -18,23 +18,42 @@ window.addEventListener('DOMContentLoaded', () => {
 	})
 }) */
 
-$(document).ready(function () {
-	$('.carousel__inner').slick({
-		// dots: true,
-		infinite: true,
-		speed: 1000,
-		slidesToShow: 1,
-		// adaptiveHeight: true,
-		prevArrow: '<button type="button" class="slick-prev"><img src="../src/icons/chevron_left.png"></button>',
-		nextArrow: '<button type="button" class="slick-next"><img src="../src/icons/chevron_right.png"></button>',
-		responsive: [
-			{
-				breakpoint: 992,
-				settings: {
-					dots: true,
-					arrows: false,
-				}
-			}
-		]
-	});
+// Slick Slider
+
+// $(document).ready(function () {
+// 	$('.carousel__inner').slick({
+// 		// dots: true,
+// 		infinite: true,
+// 		speed: 1000,
+// 		slidesToShow: 1,
+// 		// adaptiveHeight: true,
+// 		prevArrow: '<button type="button" class="slick-prev"><img src="../src/icons/chevron_left.png"></button>',
+// 		nextArrow: '<button type="button" class="slick-next"><img src="../src/icons/chevron_right.png"></button>',
+// 		responsive: [
+// 			{
+// 				breakpoint: 992,
+// 				settings: {
+// 					dots: true,
+// 					arrows: false,
+// 				}
+// 			}
+// 		]
+// 	});
+// });
+
+const slider = tns({
+	container: '.carousel__inner',
+	items: 1,
+	slideBy: 'page',
+	autoplay: false,
+	controls: false,
+	nav: false,
+	speed: 1000,
+});
+
+document.querySelector('.prev').addEventListener('click', function () {
+	slider.goTo('prev')
+});
+document.querySelector('.next').addEventListener('click', function () {
+	slider.goTo('next')
 });
